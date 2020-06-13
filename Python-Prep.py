ff16c891f1881471d45d31195ea85f4449c13898
akecho <52334046+akecho@users.noreply.github.com>
2020-06-13 10:54:44 -0700

    Create Python Prep
    Brush up on Python basics

---
+++ Python-Prep.py
@@ -0,0 +1,18 @@
+
+print('Hello, World')
+
+
+list = [4, 3, 4, 4, 3, 5, 5]
+
+random_number = 57
+for val in list:
+    print('val is', val)
+    random_number = random_number + 1
+    print('random num is', random_number)
+
+    
+i = 0
+while i < 10:
+    print(i)
+    
+    i = i + 1
